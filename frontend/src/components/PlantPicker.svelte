8de258a108167590c3576334f10b10e4391464c3
Trzcin <trzccin@gmail.com>
2023-06-03 20:01:51 +0200

    fix plant pick menu

--- frontend/src/components/PlantPicker.svelte
+++ frontend/src/components/PlantPicker.svelte
@@ -14,19 +14,19 @@
 </script>
 
 {#if !displayMenu}
-    <button on:click={() => displayMenu = true} class="absolute bottom-3 right-3 rounded shadow flex items-center space-x-2 px-4 py-2 bg-white">
+    <button on:click={() => displayMenu = true} class="absolute bottom-3 right-3 rounded shadow flex items-center space-x-2 px-4 py-2 bg-white justify-center" style="min-width: 150px;">
         <img src={currentIcon} alt="current plant">
         <p>{plantType}</p>
     </button>
 {:else}
-    <div class="absolute bottom-3 right-3 rounded shadow bg-white overflow-y-auto" style="max-height: 70vh;">
+    <div class="absolute bottom-3 right-3 rounded shadow bg-white overflow-y-auto" style="max-height: 70vh; min-width: 150px;">
         <button on:click={() => displayMenu = false} class="mt-2 ml-2">
-            close
+            <img src="/back.svg" alt="back">
         </button>
         <ul class="mt-6">
             {#each plantNames as name}
                 <li>
-                    <button on:click={() => selectPlant(name)} class="flex items-center px-4 py-2 space-x-2">
+                    <button on:click={() => selectPlant(name)} class="flex items-center px-4 py-2 space-x-2 justify-center">
                         <img src={plants[name]} alt={name}>
                         <p>{name}</p>
                     </button>
