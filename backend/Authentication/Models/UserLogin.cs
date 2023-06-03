ec9cbce060c6d805c9d429cb52993ac67dea73b8
Mateusz Chmielewski <mateusz.chmielewski1@protonmail.com>
2023-06-03 15:14:29 +0200

    Add token authorization

--- backend/Authentication/Models/UserLogin.cs
+++ backend/Authentication/Models/UserLogin.cs
@@ -8,7 +8,7 @@ namespace Authentication.Models
 {
     public class UserLogin
     {
-        public string UserName { get; set; }
-        public string Password { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
     }
 }
