cb129fccd48ab79e7ed28c1ff51d4a4dc7dfe2ae
Mateusz Chmielewski <mateusz.chmielewski1@protonmail.com>
2023-06-03 16:26:59 +0200

    Add EFDataAccessLibrary

---
+++ backend/EFDataAccessLibrary/DataAccess/ManagerContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFDataAccessLibrary.DataAccess
+{
+    public class ManagerContext : DbContext
+    {
+        public ManagerContext(DbContextOptions options) : base(options) { }
+
+    }
+}
