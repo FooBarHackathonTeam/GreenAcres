c474cb24a92ab13f43ad696ebdb3ff7e7a3eb78c
Trzcin <trzccin@gmail.com>
2023-06-03 09:10:53 +0200

    display console errors in alerts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,8 +1,12 @@
-import './app.css'
-import App from './App.svelte'
+import './app.css';
+import App from './App.svelte';
+
+window.onerror = (message, url, line) => {
+    alert(`${message}, ${url}, ${line}`);
+};
 
 const app = new App({
     target: document.getElementById('app'),
-})
+});
 
-export default app
+export default app;
