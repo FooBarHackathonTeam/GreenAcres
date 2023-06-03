c3ce139126da6937c1a2fe05ccdfde41bdd1ef27
Trzcin <trzccin@gmail.com>
2023-06-03 15:33:58 +0200

    maybe added go to user location

--- frontend/src/components/Map.svelte
+++ frontend/src/components/Map.svelte
@@ -14,6 +14,18 @@
             center: [18.6667, 54.35],
             zoom: 13
         });
+
+        map.addControl(
+            new mapboxgl.GeolocateControl({
+                positionOptions: {
+                    enableHighAccuracy: true
+                },
+                // When active the map will receive updates to the device's location as it changes.
+                trackUserLocation: true,
+                // Draw an arrow next to the location dot to indicate which direction the device is heading.
+                showUserHeading: true
+            })
+        );
     }
 </script>
 
