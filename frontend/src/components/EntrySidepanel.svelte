0d722964f87eed1174d6d8aeecd77aedf6d51fc1
Trzcin <trzccin@gmail.com>
2023-06-03 23:01:06 +0200

    add side panel with extra info

---
+++ frontend/src/components/EntrySidepanel.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import type { Entry } from "../types/Entry";
+    import { plants } from "../types/Plant";
+
+    export let entry: Entry;
+</script>
+
+<div class="h-screen w-48 bg-white px-4 py-2">
+    <div class="flex items-center space-x-2">
+        <img src={plants[entry.plantName]} alt={entry.plantName}>
+        <h1 class="text-xl">{entry.plantName}</h1>
+    </div>
+    <p class="mt-4">Some usefull data</p>
+    <img src="https://media.tenor.com/x8v1oNUOmg4AAAAd/rickroll-roll.gif" alt="xd">
+</div>
--- frontend/src/components/Map.svelte
+++ frontend/src/components/Map.svelte
@@ -5,6 +5,7 @@
     import type { Entry } from "../types/Entry";
     import type { PlantName } from "../types/Plant";
     import PlantPicker from "./PlantPicker.svelte";
+    import EntrySidepanel from "./EntrySidepanel.svelte";
 
     onMount(() => {
         setupMap();
@@ -40,13 +41,26 @@
                 geoJSON: JSON.stringify(e.features[0]),
                 plantName: currentPlant
             }];
-            alert(JSON.stringify(e.features))
+        });
+
+        map.on('draw.selectionchange', (e) => {
+            if (e.features.length == 0) {
+                selectedEntry = undefined;
+                return;
+            }
+            selectedEntry = plantEntries.find(x => x.id == e.features[0].id);
         });
     }
 
     let currentPlant: PlantName = 'wheat'; 
     let plantEntries: Entry[] = [];
+    let selectedEntry: Entry | undefined = undefined;
 </script>
 
-<div id="map-here" class="w-screen h-screen"></div>
+<div class="flex">
+    {#if selectedEntry}
+        <EntrySidepanel entry={selectedEntry}/>
+    {/if}
+    <div id="map-here" class="w-screen h-screen"></div>
+</div>
 <PlantPicker bind:plantType={currentPlant}/>
