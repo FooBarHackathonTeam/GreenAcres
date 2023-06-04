038f7b828bbc86df46cba7570193b5c5cbbf0c15
NikoHOME <wiktor.gawronski@gmail.com>
2023-06-04 12:24:43 +0200

    Update linux docker build files and script

--- backend/run.sh
+++ backend/run.sh
@@ -1,5 +1,6 @@
 rm ./docker-compose.yml
 rm ./docker-compose.override.yml
-rm .RestApi/DockerFile
+rm ./RestApi/DockerFile
 
-cp -r ./DockerBuildData/Linux/* ./
+cp -r ./DockerBuildData/Linux/docker-compose* ./
+cp -r ./DockerBuildData/Linux/DockerFile ./RestApi
